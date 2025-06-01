2b79f45c016f5cbbea1deb7cbe1854d1118b344d
dkmiec <dawidplays2007@gmail.com>
2025-06-01 17:32:33 +0100

    start again

--- tree.c
+++
@@ -1,9 +0,0 @@
-// tree has 2 branches, left and right
-
-typedef struct tree_node {
-    char value;
-    tree_node * left;
-    tree_node * right;
-} tree_node;
-
-// make a function where you can dsee the tree
